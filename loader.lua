b043ecd37169ae9ef09705572372df7d27207054
uzu01 <77998551+uzu01@users.noreply.github.com>
2022-10-05 21:34:49 +0800

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,20 @@
+local User, Repo, Folder = "uzu01", "public", "games"
+local ScriptFormat = "https://raw.githubusercontent.com/%s/%s/main/%s/%s/script.lua"
+
+local GameTable = {
+    [10723695195] = "aes",
+}
+
+local IsSupported = GameTable[game.PlaceId] or GameTable[game.GameId]
+
+if not IsSupported then return end
+if not game.Loaded then game.Loaded:Wait() end
+
+local Players = game:GetService("Players")
+local VirtualUser = game:GetService("VirtualUser")
+
+Players.LocalPlayer.Idled:Connect(function()
+    VirtualUser:ClickButton2(Vector2.new(0,0))
+end)
+
+loadstring(game:HttpGet(ScriptFormat:format(User, Repo, Folder, IsSupported)))()
