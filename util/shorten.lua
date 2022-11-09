ea7cd654225c0fe0e8e775552aea3f075f23f825
uzu01 <77998551+uzu01@users.noreply.github.com>
2022-11-09 19:17:38 +0800

    Create shorten.lua

---
+++ util/shorten.lua
@@ -0,0 +1,14 @@
+-- https://devforum.roblox.com/u/Krystaltinan
+
+return function(Number)
+	local Suffixes = {"","K","M","B","T","Q","QN","S","SP","O","N","D","UD","DD"}
+	
+	local Index = math.floor(math.log10(Number))
+	Index = Index - (Index % 3)
+
+	local Suffix = Suffixes[(Index / 3) + 1]
+	local NearestMultiple = 10 ^ Index
+	local PrecisionMultiple = 10 ^ 2
+	
+	return math.floor((Number / NearestMultiple) * PrecisionMultiple) / PrecisionMultiple .. Suffix
+end
